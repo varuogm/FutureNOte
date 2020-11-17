8567e9268c0b84e1e598661124c87f5fd6bd546d
gourav majee <44859494+varuogm@users.noreply.github.com>
2020-11-18 00:43:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+  $("#hidden1").hover(function() {
+    $(this).css("color","black");
+  },
+  function()  {
+   $(this).css("color","grey");
+  });
+});
+
+// $(document).ready(function(e) {
+//   $("h1").hover(function(){
+//     $(this).css("background-color", "yellow");
+//     }, function(){
+//     $(this).css("background-color", "pink");
+//   });
+//    // your code here
+// });
+$(document).ready(function() {
+  $("#hidden2").hover(function() {
+    $(this).css("color","black");
+  },
+  function()  {
+   $(this).css("color","grey");
+  });
+});
+
+$(document).ready(function() {
+  $("#hidden3").hover(function() {
+    $(this).css("color","black");
+  },
+  function()  {
+   $(this).css("color","grey");
+  });
+});
+
+$(document).ready(function() {
+  $("#hidden4").hover(function() {
+    $(this).css("color","black");
+  },
+  function()  {
+   $(this).css("color","grey");
+  });
+});
+
+$(document).ready(function() {
+  $("#hidden5").hover(function() {
+    $(this).css("color","black");
+  },
+  function()  {
+   $(this).css("color","grey");
+  });
+});
